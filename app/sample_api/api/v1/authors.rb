f54308ae2ed62bd51e25cbccfcad67cb01a855f7
Tristan <tristan.gomez@gmail.com>
2014-08-05 16:40:52 +0800

    api for creation of posts and authors

--- app/sample_api/api/v1/authors.rb
+++ app/sample_api/api/v1/authors.rb
@@ -9,6 +9,18 @@ module API
         get do
           Author.all
         end
+
+        desc 'creates an author'
+        params do
+          requires :name, desc: 'the author\'s name'
+        end
+        post do
+          author = ::Author.where({
+            name: params[:name],
+          }).first_or_create
+          author
+        end
+
       end
     end
   end
--- app/sample_api/api/v1/posts.rb
+++ app/sample_api/api/v1/posts.rb
@@ -9,6 +9,27 @@ module API
         get do
           Post.recent.all
         end
+
+        desc 'creates a post'
+        params do
+          requires :author_id, desc: 'the author ID (note: if the author ID does
+            not exist, it will automatically create the author model and assign
+            it a random name'
+          requires :title, desc: 'the post title'
+          requires :body, desc: 'the post body'
+        end
+        post do
+          author = ::Author.where({
+            author_id: params[:author_id],
+          }).first_or_create
+          ::Post.create(
+            author: author,
+            title: title,
+            body: body
+          )
+          post
+        end
+
       end
     end
   end
